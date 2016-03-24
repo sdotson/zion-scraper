fcf12906c619a1d4c750f40bf16887e5f2ba7293
sdotson <stu.dotson@gmail.com>
2016-03-24 18:04:16 -0400

    create hikes module

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var casper = require('casper').create({
 });
 
 var campsites = require('./lib/campsites');
+var hikes = require('./lib/hikes');
 
 var narrowsUrl = 'https://zionpermits.nps.gov/wilderness.cfm?TripTypeID=1';
 
@@ -28,30 +29,6 @@ var watchmanDates = [
 	}
 ];
 
-function getAvailability() {
-    var rows = document.querySelectorAll('#cs_idCell2x1x1 table tr:nth-of-type(10) table tr:nth-of-type(6) td');
-    var jobs = [];
-    for (var i = 0; i < rows.length; i++) {
-        var span = rows[i].querySelector('span');
-        var p = rows[i].querySelector('p');
-        var job = {};
-        job['dayNumber'] = span.innerText;
-        job['slotsAvailable'] = p.innerText;
-        jobs.push(job);
-    }
-    return jobs;
-}
-
-function getTheMonth() {
-	var month = document.querySelectorAll('#cs_idCell2x1x1 table tr:nth-of-type(10) table tr:first-child th')[0];
-	return month.innerText;
-}
-
-function getSiteName() {
-	var name = document.querySelectorAll('#cs_idCell2x1x1 table tr:nth-of-type(6) strong')[0];
-	return name.innerText;
-}
-
 var results = {
 	narrows: {
 		available: [],
@@ -63,43 +40,9 @@ var results = {
 	}
 };
 
-function checkPermits(options) {
-
-	options.permits.forEach(function(campsite) {
-
-		casper.waitForSelector('#chgresourceform', function() {
-			this.fill('form#chgresourceform', {
-		        'ResourceID' : campsite
-		    }, true);
-		});
-
-		casper.waitForSelector("#cs_idCell2x1x1 table tr table", function () {
-		    var days = this.evaluate(getAvailability),
-			    monthCaptured = this.evaluate(getTheMonth),
-			    siteName = this.evaluate(getSiteName),
-			    daysAvailable = days.filter(function(day) { return day.slotsAvailable > 0; });
-
-			results[options.label].monthCaptured = monthCaptured;
-
-			site = {
-				siteName: siteName,
-				daysAvailable: daysAvailable,
-				days: days
-			};
-
-			if (daysAvailable.length) {
-				results[options.label].available.push(site);
-			} else {
-				results[options.label].unavailable.push(site);
-			}
-		});
-	});
-	return results;
-}
-
 casper.start(narrowsUrl)
 	.then(function() {
-		return checkPermits({permits: narrowsSites,label: 'narrows'});
+		return hikes.checkPermits({permits: narrowsSites,label: 'narrows'});
 	})
 	.thenOpen(watchmanUrl)
 	.then(function() {
