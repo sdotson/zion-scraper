b49f606010a2bc521658a3b0f353b3146ba57967
sdotson <stu.dotson@gmail.com>
2016-03-28 13:47:09 -0400

    remove placeholder alert function

--- checkZion.js
+++ checkZion.js
@@ -4,11 +4,6 @@ var emails = require('./lib/emails');
 
 var result = '';
 
-function sendAlert(thing) {
-	/* will eventually be a sparkPost API call to send an email */
-	console.log('Available permits/reservations have been detected for ' + thing);
-}
-
 casperChild.stdout.on('data', function (data) {
     result += data.toString();
 });
@@ -18,7 +13,6 @@ casperChild.stdout.on('end', function () {
     var resultsObj = JSON.parse(result);
 
     if (resultsObj.narrows.available.length || resultsObj.watchman.available.length) {
-    	sendAlert('narrows');
         emails.sendEmail(resultsObj);
     } else {
         console.log('no new cancellations detected.');
