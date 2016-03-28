2561be51c95f03629500482235106262ccd4d26a
sdotson <stu.dotson@gmail.com>
2016-03-28 17:20:12 -0400

    add API

--- checkZion.js
+++ checkZion.js
@@ -6,41 +6,43 @@ var Result = require('./models/Result');
 
 var casperOutput = '';
 
-casperChild.stdout.on('data', function (data) {
-    casperOutput += data.toString();
-});
+function runCasperTasks() { 
+  casperChild.stdout.on('data', function (data) {
+      casperOutput += data.toString();
+  });
 
-casperChild.stdout.on('end', function () {
-    var resultsObj = JSON.parse(casperOutput),
-        twoHoursAgo = new Date(new Date().getTime() - 7200000), // two hours ago
-        cancellationDetected = false;
+  casperChild.stdout.on('end', function () {
+      var resultsObj = JSON.parse(casperOutput),
+          twoHoursAgo = new Date(new Date().getTime() - 7200000), // two hours ago
+          cancellationDetected = false;
 
-    if (resultsObj.narrows.available.length || resultsObj.watchman.available.length) {
-        Result.find({ cancellationDetected: true }).where('created_at').gt(twoHoursAgo).exec(function(err, results) {
-          if (err) throw err;
+      if (resultsObj.narrows.available.length || resultsObj.watchman.available.length) {
+          Result.find({ cancellationDetected: true }).where('created_at').gt(twoHoursAgo).exec(function(err, results) {
+            if (err) throw err;
 
-          if (!results.length) {
-            emails.sendEmail(resultsObj);
-          }
-        });
-        cancellationDetected = true;
-    } else {
-        console.log('No new cancellations detected.');
-    }
+            if (!results.length) {
+              emails.sendEmail(resultsObj);
+            }
+          });
+          cancellationDetected = true;
+      } else {
+          console.log('No new cancellations detected.');
+      }
 
-    var newResult = Result({
-      cancellationDetected: cancellationDetected,
-      results: resultsObj,
-      created_at: new Date()
-    });
+      var newResult = Result({
+        cancellationDetected: cancellationDetected,
+        results: resultsObj,
+        created_at: new Date()
+      });
 
-    newResult.save(function(err) {
-      if (err) throw err;
-      console.log('CasperJS result logged!');
-      mongoose.disconnect();
-    });
-});
+      newResult.save(function(err) {
+        if (err) throw err;
+        console.log('CasperJS result logged!');
+        mongoose.disconnect();
+      });
+  });
+}
 
 
-
+module.exports = runCasperTasks;
 
--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var schedule = require('node-schedule');
+var express = require('express');
+var app = express();
+
+var Result = require('./models/Result');
+
 var checkZion = require('./checkZion');
 
-
 var j = schedule.scheduleJob('*/30 * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
   checkZion();
+  console.log('checkZion() ran.');
 });
+
+app.get("/logs/all", function(req, res) {
+	Result.find(function(err, results) {
+	  if (err) return console.error(err);
+		res.json(results);
+	});
+});
+
+app.get("/logs/last", function(req, res) {
+	Result.find().sort('-created_at').limit(1).exec(function(err, results){
+	    res.json(results);
+	});
+});
+
+app.get("/logs/last10", function(req, res) {
+	Result.find().sort('-created_at').limit(10).exec(function(err, results){
+	    res.json(results);
+	});
+});
+
+app.listen(3000);
+console.log('Starting server on port 3000');
