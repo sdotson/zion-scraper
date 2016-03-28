84d2b26dc8d9845afc2ea6b261345589dd0857ea
sdotson <stu.dotson@gmail.com>
2016-03-28 14:45:06 -0400

    add mongo and mongoose for record of results

--- checkZion.js
+++ checkZion.js
@@ -1,23 +1,46 @@
 var spawn = require('child_process').spawn;
 var casperChild = spawn('casperjs', ['casperTasks.js']);
+var mongoose = require('mongoose');
 var emails = require('./lib/emails');
+var Result = require('./models/Result');
 
-var result = '';
+var casperOutput = '';
 
 casperChild.stdout.on('data', function (data) {
-    result += data.toString();
+    casperOutput += data.toString();
 });
 
 casperChild.stdout.on('end', function () {
-	console.log(result);
-    var resultsObj = JSON.parse(result);
+    console.log(casperOutput);
+    var resultsObj = JSON.parse(casperOutput),
+        cancellationDetected = false;
 
     if (resultsObj.narrows.available.length || resultsObj.watchman.available.length) {
         emails.sendEmail(resultsObj);
+        cancellationDetected = true;
     } else {
-        console.log('no new cancellations detected.');
+        console.log('No new cancellations detected.');
     }
 
+    var newResult = Result({
+      cancellationDetected: false,
+      results: resultsObj,
+      created_at: new Date()
+    });
+
+    newResult.save(function(err) {
+      if (err) throw err;
+      console.log('Result created!');
+      mongoose.disconnect();
+    });
+
+    /*Result.find({}, function(err, results) {
+      if (err) throw err;
+      console.log(results);
+    });*/
+
 });
+
+
 
 
---
+++ models/Result.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+process.on('SIGINT', function() {  
+  mongoose.connection.close(function () { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+}); 
+
+mongoose.connect('mongodb://localhost/zion');
+
+var Schema = mongoose.Schema;
+
+var resultSchema = new Schema({
+  results: Object,
+  cancellationDetected: Boolean,
+  created_at: Date,
+});
+
+var Result = mongoose.model('Result', resultSchema);
+
+module.exports = Result;
